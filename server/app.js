e241d5e0c993011c1ae2ad26f88f3d877b7b7fa4
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-10-09 09:54:51 -0600

    parse url

--- server/app.js
+++ server/app.js
@@ -1,12 +1,17 @@
 const http = require('http');
+const url = require('url');
 const config = require('./config');
 
 
-// Server setup
+// Server initiation & setup
 const server = http.createServer((req, res) => {
-  res.end('Hellow world\n');
+  // Gets the URL, parse it, & trimm the leading and ending '/'
+  const trimmedPath = url.parse(req.url, true).pathname.replace(/^\/+|\/+$/g,'');
+
+  // Send response
+  res.end(`Hello world\n You requested path: ${trimmedPath}`);
 });
 
 server.listen(config.httpPort, ()=> {
-  console.log(`Server is listening on port ${config.httpPort}`);
+  console.log(`Server is listening on port ${config.httpPort}.`);
 })
